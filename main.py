0d68d63f0c628a1d08a421fdf3379fe07a8ad8d4
mrnn2004 <1383mrnn@gmail.com>
2022-07-14 20:04:05 +0430

    eddited music search

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from ytmusicapi import YTMusic
 
 ytmusic = YTMusic('headers_auth.json')
 playlistId = ytmusic.create_playlist('test', 'test description')
-search_results = ytmusic.search('Oasis Wonderwall')
+search_results = ytmusic.search('7rings')
+print(search_results)
 ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
 
 
