2b7da13a41e43b4e3b0f584c73e6cbca48255130
mohammad reza <1383mrnn@gmail.com>
2022-07-14 16:02:42 +0430

    added serch_result json file

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from ytmusicapi import YTMusic
 ytmusic = YTMusic('headers_auth.json')
 playlistId = ytmusic.create_playlist('test', 'test description')
 search_results = ytmusic.search('7rings')
-print(search_results)
+f = open('search_result.json', 'w')
+f.write(str(search_results))
+f.close
 ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
 
 
