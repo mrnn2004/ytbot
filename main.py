1bd2798a7acead2538c618b528a5318764f38792
mohammad reza <1383mrnn@gmail.com>
2022-07-15 14:28:00 +0430

    added pytube library and test it

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from ytmusicapi import YTMusic
+import json
+from pytube import YouTube
+
 
 ytmusic = YTMusic('headers_auth.json')
 playlistId = ytmusic.create_playlist('test', 'test description')
 search_results = ytmusic.search('7rings')
-f = open('search_result.json', 'w')
-f.write(str(search_results))
-f.close
-ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
+
+aList = search_results
+jsonStr = json.dumps(aList)
+print(jsonStr)
 
 
+
+ytmusic.add_playlist_items(playlistId, [search_results[1]['videoId']])
+
+print('-------------------------------------------------------------------------------------------------------')
+
+yt = YouTube('https://music.youtube.com/watch?v=XZ868t23Pb4')
+
+print(yt.streams)
